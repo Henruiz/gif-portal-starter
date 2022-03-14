817b17e0d730ccb4f762951f4fc0838d89317942
hruiz1905@gmail.com <hruiz1905@gmail.com>
2022-03-13 23:11:31 -0500

    Section 1 Lesson 3: Connecting Phantom Wallet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'wickdxd';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+/*
+  name: checkIfWalletIsConnected
+  params: none
+  return: message
+*/
+const checkIfWalletIsConnected = async () => {
+  try {
+    const { solana } = window;
+
+    if (solana) {
+      if (solana.isPhantom) {
+        console.log('Phantom wallet found!');
+      }
+      /*
+       * The solana object gives us a function that will allow us to connect
+       * directly with the user's wallet!
+      */
+      const response = await solana.connect({ onlyIfTrusted: true });
+      console.log(
+        'Connected with Public Key:',
+        response.publicKey.toString()
+      );
+    } else {
+      alert('Solana Phantom Wallet not found! Please install Phantom Wallet 👻');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/* place holder for now */
+const connectWallet = async () => {};
+
+/* rendering UI when user has not connected their wallet */
+const renderNotConnectedContainer = () => (
+  <button
+    className="cta-button connect-wallet-button"
+    onClick={connectWallet}
+  > Connect to Wallet
+  </button>
+);
+
+/*
+  Add evenListener for Phantom Wallet
+*/
+useEffect(() => {
+  const onLoad = async () => {
+    await checkIfWalletIsConnected();
+  };
+  window.addEventListener('load', onLoad);
+  return () => window.removeEventListener('load', onLoad);
+}, []);
+
+
+  // This renders the front end
   return (
     <div className="App">
       <div className="container">
@@ -14,6 +71,7 @@ const App = () => {
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
